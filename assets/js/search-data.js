bfb9fc1b85dea8d0777db8bc739c21587c93aff2
temuulun <temulun_b612@outlook.com>
2025-09-22 16:36:55 +0000

    Deploying to gh-pages from @ temuulun/temuulun.github.io@7dbcbe863d37636e006d3892d00a8f0e18c0bb2f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "dropdown-gallery",
-              title: "gallery",
+              title: "Gallery",
               description: "",
               section: "Dropdown",
               handler: () => {
