efc56f81ca8b69feb01a832ba8b4e705bbf8ed1b
temuulun <temulun_b612@outlook.com>
2025-09-22 12:47:39 +0000

    Deploying to gh-pages from @ temuulun/temuulun.github.io@aaf5a3bd91b5e64b36b4604ac090a359165fa5fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "A list of institutions I&#39;ve spent time at.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "Collection of working papers",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "A list of institutions I&#39;ve spent time at.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "dropdown-gallery",
               title: "gallery",
