77a3a9090feeac0ca283ae28ce5a9b8c959728f8
temuulun <temulun_b612@outlook.com>
2025-09-24 16:54:27 +0000

    Deploying to gh-pages from @ temuulun/temuulun.github.io@0ff2805cdaa4687458b8a4362c63abc04a40caf4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,4 +30,14 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/gallery/";
               },
-            },];
+            },{id: "photography-portraits",
+          title: 'Portraits',
+          description: "",
+          section: "Photography",handler: () => {
+              window.location.href = "/gallery/";
+            },},{id: "photography-portraits",
+          title: 'Portraits',
+          description: "",
+          section: "Photography",handler: () => {
+              window.location.href = "/gallery/";
+            },},];
